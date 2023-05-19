28d91c884a3dea8bd35bb3123b15501ecc65420d
Rippelzin <gabrielrippelima@gmail.com>
2023-05-19 13:37:54 -0300

    avancando em django-views & urls

---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Gabriel',
+    })
